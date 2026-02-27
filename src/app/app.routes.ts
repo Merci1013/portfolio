56f6b357d77b8977a2eb6943b72dce5ed3b1df29
Merci Dikeko Nsingi <merci.nsingi-dikeko@polymtl.ca>
2026-02-26 20:18:48 -0500

    Correction du bug sur l'admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { CvComponent } from './pages/cv/cv';
 import { ProjectsComponent } from './pages/projects/projects';
 import { ContactComponent } from './pages/contact/contact';
 import { InteretsComponent } from './pages/interets/interets';
-import { AdminComponent } from './pages/admin/admin';
 import { adminGuard } from './auth/admin.guard';
 
 export const routes: Routes = [
@@ -13,6 +12,5 @@ export const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'interets', component: InteretsComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
   { path: '**', redirectTo: '' },
 ];
